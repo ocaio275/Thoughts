370178c8cd0cf72a732c2666ea97f11d471a54c1
ocaio275 <ocaio275@gmail.com>
2022-08-18 15:36:02 -0300

    :sparkles: feat: criando flash message para view 'Registro'

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,10 +1,24 @@
-module.exports = class AuthController{
-    static login(req, res){
+const User = require('../Models/User')
+
+const bcrypt = require('bcryptjs')
+module.exports = class AuthController {
+    static login(req, res) {
         res.render('auth/login')
     }
 
-    static register(req, res){
+    static register(req, res) {
         res.render('auth/register')
     }
 
+    static async registerPost(req, res) {
+        const { name, email, password, confirmpassword } = req.body
+
+        // Password matcg validation
+
+        if (password != confirmpassword) {
+            req.flash('message', 'As senhas não conferem, tente novamente')
+            res.render('auth/register')
+            return
+        }
+    }
 }
--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -4,5 +4,6 @@ const AuthController = require('../Controllers/AuthController')
 
 AuthRoutes.get('/login', AuthController.login)
 AuthRoutes.get('/register', AuthController.register)
+AuthRoutes.post('/register', AuthController.registerPost)
 
 module.exports = AuthRoutes
